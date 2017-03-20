c1ea92eb4b7d421189db5c3447c400c53b969b84
wosp <wolfgang.spiess@fhws.de>
2017-03-20 15:39:33 +0100

    Create Project

--- src/ollehpackage/OllehDlrowClass.java
+++ src/ollehpackage/OllehDlrowClass.java
@@ -6,8 +6,9 @@ abstract class OllehDlrowClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    System.out.println("Hello git");
     System.out.println("Hello world");
+    System.out.println("Hello Git");
+  
 	}
 
 }
