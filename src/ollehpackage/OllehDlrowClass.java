01613bb7ecf971686395d33278712011a33a7322
wosp <wolfgang.spiess@fhws.de>
2017-03-20 15:21:46 +0100

    Create Project

---
+++ src/ollehpackage/OllehDlrowClass.java
@@ -0,0 +1,12 @@
+package ollehpackage;
+/*
+ * Florian Spiess  19.03.2017 
+ */
+abstract class OllehDlrowClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("Hello world");
+	}
+
+}
